e8043e1d6805e59b0fbfe27d7af32b3250bb840d
Михаил <@DESKTOP-ND5PD19>
2023-11-01 20:00:49 +0300

    some changes

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,4 @@
 from app.routers.auth import router as register_router
-from app.routers.invitation_workplace import router as invitation_workplace_router
 from app.routers.issue import router as issue_router
 from app.routers.sprint import router as sprint_router
 from app.routers.workplace import router as workplace_router
@@ -9,7 +8,6 @@ list_of_routes = [
     workplace_router,
     sprint_router,
     issue_router,
-    invitation_workplace_router,
 ]
 
 
--- app/routers/workplace.py
+++ app/routers/workplace.py
@@ -4,11 +4,15 @@ from uuid import UUID
 
 from beanie import DeleteRules, WriteRules
 from beanie.operators import In, RegEx
-from fastapi import APIRouter, Body, Depends, Path, UploadFile, status
-from fastapi.responses import FileResponse
+from fastapi import APIRouter, BackgroundTasks, Body, Depends, Path, Request, UploadFile, status
+from fastapi.responses import FileResponse, RedirectResponse
+from pydantic import EmailStr
 
-from app.auth.oauth2 import admin, get_current_user, guest, member
+from app.auth.jwt_token import decode_token
+from app.auth.oauth2 import admin, get_current_user, guest, member, oauth2_scheme
+from app.config import client_api_settings
 from app.core.download import downloader
+from app.core.email import Email
 from app.core.exceptions import WorkplaceFileNotFoundException
 from app.schemas.documents import Role, User, UserAssignedWorkplace, Workplace
 from app.schemas.models import FileModelOut, SuccessfulResponse, WorkplaceCreation
@@ -96,3 +100,34 @@ async def get_file(
     if not pathlib.Path.is_file(path_file):
         raise WorkplaceFileNotFoundException("Файл не найден")
     return FileResponse(path_file)
+
+
+@router.get("/workplaces/{workplace_id}/invitation", status_code=status.HTTP_200_OK)
+async def add_to_workplace(workplace_id: UUID = Path(...), token: UUID = Depends(oauth2_scheme)):
+    token = decode_token(token.credentials)
+    user = await User.by_email(token.email)
+
+    # Если пользователь не вошёл или не зарегистрировался
+    if not user:
+        return RedirectResponse(client_api_settings.MAIN_URL)
+
+    # Если всё хорошо
+    workplace = await Workplace.find_one(Workplace.id == workplace_id)
+    workplace.users.append(UserAssignedWorkplace(user=user, workplace_id=workplace.id, role=Role.MEMBER))
+    await workplace.save(link_rule=WriteRules.WRITE)
+
+    return RedirectResponse(f"/workplaces/{workplace_id}")
+
+
+@router.post("/workplaces/{workplace_id}/invite", response_model=SuccessfulResponse, status_code=status.HTTP_200_OK)
+async def invite_to_workplace(
+    request: Request,
+    background_tasks: BackgroundTasks,
+    email: Email = Depends(Email),
+    new_user_email: EmailStr = Body(...),
+    workplace_id: UUID = Path(...),
+    user: UserAssignedWorkplace = Depends(admin),
+):
+    workplace = await Workplace.find_one(Workplace.id == workplace_id)
+    background_tasks.add_task(email.sendInvitationMail, request, new_user_email, workplace_id, workplace.name)
+    return SuccessfulResponse()
