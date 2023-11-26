b53ef4e7ca245120f709c3ba8de8ddc4ac33f36b
mimalakhov <94488402+mimalakhov@users.noreply.github.com>
2023-11-26 17:37:28 +0300

    fixes (#21)
    Co-authored-by: Михаил <Михаил@DESKTOP-ND5PD19>

--- app/schemas/documents.py
+++ app/schemas/documents.py
@@ -113,6 +113,7 @@ class Issue(Document, IssueBase):
     sprint: BackLink["Sprint"] = Field(default=None, original_field="issues", exclude=True)
     workplace_id: UUID = Field(exclude=True)
     sprint_id: UUID = Field(exclude=True)
+    files: List[str] = Field(default_factory=list)
 
     @before_event(Delete)
     async def delete_refs(self):
@@ -135,6 +136,7 @@ class Comment(Document, CommentCreation):
     workplace_id: UUID = Field(exclude=True)
     sprint_id: UUID = Field(exclude=True)
     issue_id: UUID = Field(exclude=True)
+    files: List[str] = Field(default_factory=list)
 
     @before_event(Delete)
     async def delete_refs(self):
